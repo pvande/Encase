02c4f636da345d2bb4c61085f7dba136522d9711
Pieter van de Bruggen <pvande@gmail.com>
2012-07-05 20:13:54 -0700

    Adding documentation

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@ source "https://rubygems.org"
 group :test do
   gem "rspec"
 end
+
+group :doc do
+  gem "yard"
+end
--- lib/encase/decorator.rb
+++ lib/encase/decorator.rb
@@ -1,11 +1,26 @@
 module Encase
+  # A basic implementation of the Decorator pattern.  An instance of a
+  # decorator can "wrap" a callable type (like a Proc or a Method) and augment
+  # their behavior.
   class Decorator
+
+    # Wraps the given callable object in a Proc that passes it through to the
+    # {#around} method (which itself delegates to {#before} and {#after}).
+    # @param code [#call] the callable to augment
+    # @return [Proc] a decorated callable
     def wrap_callable(code)
       decorator = self
       proc { |*args, &block| decorator.send(:around, code, args, block) }
     end
 
     private
+
+    # Called from the wrapper Proc.
+    # @api extender Intended to be an extension point for subclasses
+    # @param code [#call] the callable being augmented
+    # @param args [Array[Any]] the arguments the wrapper was invoked with
+    # @param block [Proc] the block the wrapper was invoked with
+    # @return the result of calling +code+
     def around(code, args, block)
       before(code, args, block)
       code.call(*args, &block).tap do |retval|
@@ -13,7 +28,17 @@ module Encase
       end
     end
 
+    # Called before the wrapped callable.
+    # @api extender Intended to be an extension point for subclasses
+    # @param (see #around)
     def before(code, args, block); end
+
+    # Called after the wrapped callable.
+    # @api extender Intended to be an extension point for subclasses
+    # @param code [#call] the callable being augmented
+    # @param args [Array[Any]] the arguments the wrapper was invoked with
+    # @param block [Proc] the block the wrapper was invoked with
+    # @param retval [Any] the result of having called +code+
     def after(code, args, block, retval); end
   end
 end
