fa1022c43a5162698971e2c01b0c27f8ae3ff226
Pieter van de Bruggen <pvande@gmail.com>
2014-10-11 13:03:30 -0700

    Moving the whole benchmarking code out of the specs.

--- benchmark.rb
+++ benchmark.rb
@@ -20,22 +20,57 @@ class Checked
   end
 end
 
-class Contracted
-  include Encase::Contracts
+module Contracted
+  class Unchecked
+    include Encase::Contracts
 
-  Contract Integer => Integer
-  def self.fib(n)
-    return n if n < 2
-    return fib(n - 1) + fib(n - 2)
+    Contract()
+    def self.fib(n)
+      return n if n < 2
+      return fib(n - 1) + fib(n - 2)
+    end
+  end
+
+  class ArgumentCheck
+    include Encase::Contracts
+
+    Contract Integer
+    def self.fib(n)
+      return n if n < 2
+      return fib(n - 1) + fib(n - 2)
+    end
+  end
+
+  class ReturnCheck
+    include Encase::Contracts
+
+    Contract Encase::Contracts::Returns[Integer]
+    def self.fib(n)
+      return n if n < 2
+      return fib(n - 1) + fib(n - 2)
+    end
+  end
+
+  class ArgumentAndReturnCheck
+    include Encase::Contracts
+
+    Contract Integer => Integer
+    def self.fib(n)
+      return n if n < 2
+      return fib(n - 1) + fib(n - 2)
+    end
   end
 end
 
 Benchmark.ips do |x|
-  x.config(:time => 10, :warmup => 2)
+  x.config(:time => 4, :warmup => 2)
 
   x.report("Unadorned") { Simple.fib(6) }
   x.report("Checked") { Checked.fib(6) }
-  x.report("Contracted") { Contracted.fib(6) }
+  x.report("Contracted: Empty") { Contracted::Unchecked.fib(6) }
+  x.report("Contracted: Args") { Contracted::ArgumentCheck.fib(6) }
+  x.report("Contracted: Returns") { Contracted::ReturnCheck.fib(6) }
+  x.report("Contracted: Both") { Contracted::ArgumentAndReturnCheck.fib(6) }
 
   x.compare!
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,52 +8,6 @@ unless ENV['CI'] or RUBY_VERSION =~ /^1\.8\./
 end
 
 RSpec.configure do |conf|
-  if require 'benchmark'
-    conf.after :suite do
-      puts "\n"
-
-      class BenchmarkingTest
-        def undecorated_method(x, y, z)
-          [x, y, z]
-        end
-
-        include Encase::Decorator.module
-        Decorator()
-        def with_decorator(x, y, z)
-          [x, y, z]
-        end
-
-        include Encase::Contracts
-        Contract Symbol, String, Fixnum => [ Symbol, String, Fixnum ]
-        def with_contract(x, y, z)
-          [x, y, z]
-        end
-      end
-
-      instance, n = BenchmarkingTest.new, 50_000
-      {
-        :Validated => proc { },
-        :Disabled => proc { Encase::Decorator.disable },
-      }.each do |name, code|
-        puts "\n#{name}" ; code.call()
-
-        Benchmark.bm(15) do |x|
-          x.report('undecorated:') do
-            n.times { instance.undecorated_method(:one, '2', 3) }
-          end
-
-          x.report('with decorator:') do
-            n.times { instance.with_decorator(:one, '2', 3) }
-          end
-
-          x.report('with contract:') do
-            n.times { instance.with_contract(:one, '2', 3) }
-          end
-        end
-      end
-    end
-  end
-
   conf.after(:suite) do
     puts "\n"
     SimpleCov.at_exit.call
